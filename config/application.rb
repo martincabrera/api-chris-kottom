2302c6c18a1a511e340d6c1b7dfa7bf65f4e7ed7
Martin Cabrera <martin@kisland.com>
2018-01-08 23:59:56 +0100

    updates v3

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module JelloServer
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib', 'jsonapi')
     config.api_only = true
   end
 end
